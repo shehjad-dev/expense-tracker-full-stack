84c0184cb06767b297a882d10c122c01a912b7d3
shehjad-dev <satauskz@gmail.com>
2025-04-08 04:55:23 +0600

    refactor: 💡 removed unnecessary cron.service.ts

--- backend/src/cron/cron.job.ts
+++ backend/src/cron/cron.job.ts
@@ -1,22 +1,28 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { CronService } from './cron.service';
+import { RabbitMQService } from 'src/rabbitmq/rabbitmq.service';
 
 @Injectable()
 export class CronJob {
     private readonly logger = new Logger(CronJob.name);
 
-    constructor(private readonly cronService: CronService) { }
+    constructor(private readonly rabbitMQService: RabbitMQService) { }
 
     // @Cron(CronExpression.EVERY_5_SECONDS) || @Cron('0 0 1 * *')
-    @Cron('0 0 1 * *')
+    @Cron(CronExpression.EVERY_5_SECONDS)
     async handleCron() {
         this.logger.log('Monthly cron job started');
+        const msg = {
+            id: '2938ta2938923',
+            timestamp: new Date().toISOString(),
+            text: 'This is a dummy payload',
+        };
+
         try {
-            await this.cronService.sendMessage();
-            this.logger.log('Monthly cron job completed successfully');
+            await this.rabbitMQService.sendToQueue(msg);
+            this.logger.debug('Message successfully sent to RabbitMQ queue');
         } catch (error) {
-            this.logger.error('Monthly cron job failed', error);
+            this.logger.error('Failed to send message to RabbitMQ', error);
             throw error;
         }
     }
--- backend/src/cron/cron.module.ts
+++ backend/src/cron/cron.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CronJob } from './cron.job';
-import { CronService } from './cron.service';
 import { RabbitMQModule } from '../rabbitmq/rabbitmq.module';
 
 @Module({
     imports: [RabbitMQModule],
-    providers: [CronJob, CronService],
+    providers: [CronJob],
 })
 export class CronModule { }
